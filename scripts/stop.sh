9b519d13314424b727451959791c557cc976295b
IMjaeyongpark <jaeyong36@naver.com>
2024-01-26 17:40:05 +0900

    asfasfas

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -15,6 +15,6 @@ if [ -z $CURRENT_PID ]; then
   sudo echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   sudo echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  sudo kill -15 $CURRENT_PID
   sleep 5  # 종료를 기다린 후 다음 작업 수행
 fi
